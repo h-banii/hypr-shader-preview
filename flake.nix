ae641a8987cbf27c293c0454454d01d189cf56d2
h-banii <121690516+h-banii@users.noreply.github.com>
2025-03-11 23:23:57 -0300

    nix: Add basic devshell

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  outputs =
+    {
+      nixpkgs,
+      systems,
+      ...
+    }:
+    let
+      inherit (nixpkgs) lib;
+      forAllSystems = lib.genAttrs (import systems);
+      pkgsFor = forAllSystems (system: nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = pkgsFor.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            name = "hypr-shader-preview-devshell";
+            buildInputs = with pkgs; [
+              nodejs
+              typescript-language-server
+            ];
+          };
+        }
+      );
+    };
+}
